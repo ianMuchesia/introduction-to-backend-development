91ccb9eb57ed1919bcd2c93243f35a892207987c
ianMuchesia <ianmuchesia@yahoo.com>
2023-01-09 16:29:28 +0300

    add validation to mongoose

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
-const express = require('express')
+const connectDB = require("./db/connect");
+const express = require("express");
+const app = express();
+require('dotenv').config()
 
-const app = express()
-
-const port = 3000
+const port = 3000;
 
 //middleware
-app.use(express.json())
+app.use(express.json());
 //routes
-const tasks = require("./routes/task")
+const tasks = require("./routes/task");
 
-
-app.use('/api/v1/tasks', tasks)
-app.get("/", (req, res)=>{
-    res.send("Hello world")
-})
+app.use("/api/v1/tasks", tasks);
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
 
 ///api/v1/tasks  -get all tasks
 //post('api/v1/tasks')   -create all new tasks
 //app.get('/api/v1/tasks/:id')  -get single task
 //app.patch('api.v1/tasks/:id')  -update task
 //app.delete('api/v1/tasks/;id) -delete task
- 
-app.listen(port,console.log(`server listening on port ${port}...`))
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`server listening on port ${port}..`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
